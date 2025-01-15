8bcedae8889b93f5a0f64ef06038a854cd8e979a
Navi <aderifalfauzi@gmai;.com>
2025-01-16 02:49:08 +0700

    initial relation and delivery module

---
+++ navi_for_shi/relations/doctype/customer_and_vendor/test_customer_and_vendor.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Navi and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCustomerandVendor(FrappeTestCase):
+	pass
